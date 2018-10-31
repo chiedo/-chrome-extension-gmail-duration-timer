c7bbdfe52591a6d8e00fe689bd761e437f5045d2
Chiedo <chiedo@chiedo.com>
2018-10-31 10:41:29 -0400

    Encourage phone call

--- content.js
+++ content.js
@@ -4,15 +4,15 @@ var chiedo_timeInEmails = 0;
 // Counter
 setInterval(function() {
   chiedo_timeInEmails++;
-  
-  // For each interval of X minutes, alert the user to hurry up
-  // their emails
-  
+
+  // Display time in email in the console
   if(chiedo_timeInEmails % 60 === 0) {
     console.log("You've now spent " + Math.ceil(chiedo_timeInEmails / 60) + " minutes in gmail.");
   }
 
+  // For each interval of X minutes, alert the user to hurry up
+  // their emails
   if(chiedo_timeInEmails % 600 === 0) {
-    alert("You've now spent " + Math.ceil(chiedo_timeInEmails / 60) + " minutes in gmail.");
+    alert("Phone call instead? You've now spent " + Math.ceil(chiedo_timeInEmails / 60) + " minutes in gmail.");
   }
 }, 1000);
